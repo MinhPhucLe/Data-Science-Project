ad3a2085ca433255d48658dd297cbcd15e84c9df
Bùi Phương Nam <phuongnam2442003@gmail.com>
2024-12-16 19:35:43 +0700

    done data2

---
+++ process/mapping_screen_type.py
@@ -0,0 +1,18 @@
+screen_type_mapping = {
+    'OLED': 'OLED',
+    'HD': 'HD',
+    'IPS': 'IPS',
+    '8K': '8K',
+    '2K': '2K',
+    'TFT': 'TFT',
+    'WQHD': 'WQHD',
+    'Độ phân giải:Full HD': 'Full HD',
+    'Retina': 'Retina',
+    'WUXGA': 'WUXGA',
+    '2.8K': '2.8K',
+    '3.2K': '3.2K',
+    'Độ phân giải:QHD': 'QHD',
+    '2.2K': '2.2K',
+    '2.5K': '2.5K',
+    'Độ phân giải:4K/UHD': '4K/UHD'
+}
