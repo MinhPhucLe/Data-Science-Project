f28fb8c829fec504c0e5b14368419637e6c8e6a0
Nguyen Duc Thang <103989353+GeniusVenus@users.noreply.github.com>
2024-12-17 22:48:53 +0700

    ✨ feat: add tag for some labels

--- Web/app/forms.py
+++ Web/app/forms.py
@@ -58,14 +58,16 @@ class LaptopPredictionForm(forms.Form):
     age = forms.ChoiceField(choices=AGE_CHOICES, widget=forms.Select(attrs={'class': 'form-control select2'}))
     cpu_brand = forms.ChoiceField(choices=CPU_BRAND_CHOICES, widget=forms.Select(attrs={'class': 'form-control select2'}))
     cpu = forms.ChoiceField(choices=CPU_CHOICES, widget=forms.Select(attrs={'class': 'form-control select2'}))
-    ram_capacity = forms.ChoiceField(choices=RAM_CAPACITY_CHOICES, widget=forms.Select(attrs={'class': 'form-control select2'}))
+    ram_capacity = forms.ChoiceField(choices=RAM_CAPACITY_CHOICES, widget=forms.Select(attrs={'class': 'form-control select2'}), label="Ram capacity(GB)" )
     ram_brand = forms.ChoiceField(choices=RAM_BRAND_CHOICES, widget=forms.Select(attrs={'class': 'form-control select2'}))
     hard_drive_type = forms.ChoiceField(choices=HARD_DRIVE_TYPE_CHOICES, widget=forms.Select(attrs={'class': 'form-control select2'}))
-    hard_drive_capacity = forms.ChoiceField(choices=HARD_DRIVE_CAPACITY_CHOICES, widget=forms.Select(attrs={'class': 'form-control select2'}))
+    hard_drive_capacity = forms.ChoiceField(choices=HARD_DRIVE_CAPACITY_CHOICES, widget=forms.Select(attrs={'class': 'form-control select2'}), label="Hard drive capacity(GB)")
     card_brand = forms.ChoiceField(choices=CARD_BRAND_CHOICES, widget=forms.Select(attrs={'class': 'form-control select2'}))
     card = forms.ChoiceField(choices=CARD_CHOICES, widget=forms.Select(attrs={'class': 'form-control select2'}))
     screen_size = forms.DecimalField(
     widget=forms.NumberInput(attrs={'class': 'form-control form-number-input', 'min': '0', 'step': '0.1'}),
     decimal_places=2,
-    required=False )
+    required=False,
+    label="Screen size(inches)"
+    )
     screen_type = forms.ChoiceField(choices=SCREEN_TYPE_CHOICES, widget=forms.Select(attrs={'class': 'form-control select2'}))
